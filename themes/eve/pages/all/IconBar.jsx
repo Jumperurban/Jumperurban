14b227dbcdac2ee3a57bd434d4ab2294336e81c5
Jumperurban <158606941+Jumperurban@users.noreply.github.com>
2024-03-05 20:10:18 +0100

    Update IconBar.jsx

--- themes/eve/pages/all/IconBar.jsx
+++ themes/eve/pages/all/IconBar.jsx
@@ -14,13 +14,13 @@ const IconBar = () => {
     <div>
       {showIconBar && (
         <div className="icon-bar">
-          <a href="https://www.facebook.com/mineralplanet.es/" target="_blank" rel="noopener noreferrer">
+          <a href="https://www.facebook.com/profile.php?id=61557076948178" target="_blank" rel="noopener noreferrer">
             <FaFacebookF />
           </a>
-          <a href="https://www.tiktok.com/@mineralplanet" target="_blank" rel="noopener noreferrer">
+          <a href="https://www.tiktok.com/@jumper_st?_t=8kOyOrlpNCb&_r=1" target="_blank" rel="noopener noreferrer">
             <FaTiktok />
           </a>
-          <a href="https://www.instagram.com/mineral_planet_fuengirola" target="_blank" rel="noopener noreferrer">
+          <a href="https://www.instagram.com/jumperurban/" target="_blank" rel="noopener noreferrer">
             <FaInstagram />
           </a>
           <a href="#" onClick={toggleIconBar}>
